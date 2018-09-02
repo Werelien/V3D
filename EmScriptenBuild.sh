55a758cf5924d6b9af1e1c47f9e8ce20909c8afd
gsyms <gary365@protonmail.ch>
2018-09-02 15:32:37 +0100

    .

--- EmScriptenBuild.sh
+++ EmScriptenBuild.sh
@@ -1,2 +1,3 @@
+rm -r ~/webv3d/*
 ~/emsdk/emscripten/1.38.11/em++ -v -O3 -s FULL_ES2=1 -s USE_SDL=2  V3D.cpp GFX.cpp  Strings.cpp SysSDL.cpp -o ~/webv3d/v3d.html 
 mv ~/webv3d/v3d.html ~/webv3d/index.html
